ff7fc89b10856f59da81ec78684c0cd72ddc4916
SiddhantSKarki <karkiss@miamioh.edu>
2025-03-04 20:33:01 -0500

    Started Variational Autoencoder demo notebook

--- src/lib/AutoEncoders/autoencoders.py
+++ src/lib/AutoEncoders/autoencoders.py
@@ -28,7 +28,7 @@ class FFAutoEncoder(nn.Module):
             nn.Linear(latent_dim, 50), # bottle neck
             nn.ReLU(),
             nn.Linear(50, 100),
-            nn.ReLU(),
+            nn.R`eLU(),
             nn.Linear(100, input_dim),
             nn.Sigmoid()
         )
@@ -37,3 +37,33 @@ class FFAutoEncoder(nn.Module):
         x = self.encoder(x) # encoding
         x = self.decoder(x) # reconstruction
         return x
+    
+class ConvAutoencoderMNIST(nn.Module):
+    def __init__(self, n_channels=1):
+        super(ConvAutoencoderMNIST, self).__init__()
+        
+        # Encoder
+        self.encoder = nn.Sequential(
+            nn.Conv2d(n_channels, 16, kernel_size=3, stride=2, padding=1),  
+            nn.ReLU(),
+            nn.Conv2d(16, 32, kernel_size=3, stride=2, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(32, 64, kernel_size=3, stride=2, padding=1),
+            nn.ReLU()
+        )
+        
+        # Decoder
+        self.decoder = nn.Sequential(
+            nn.ConvTranspose2d(64, 32, kernel_size=3, stride=2, padding=1, output_padding=1),
+            nn.ReLU(),
+            nn.ConvTranspose2d(32, 16, kernel_size=3, stride=2, padding=1, output_padding=1),
+            nn.ReLU(),
+            nn.ConvTranspose2d(16, n_channels, kernel_size=3, stride=2, padding=1, output_padding=1),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        B, C, H , W = x.shape
+        x = self.encoder(x)
+        x = self.decoder(x)
+        return x[...,2:-2,2:-2]
